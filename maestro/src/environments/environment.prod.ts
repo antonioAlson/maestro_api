9db9e85c16c9fe63efb5f5c80095068d9084d85c
Caio Guarino Silva <guarino.silva@opera.security>
2026-04-22 12:22:20 -0300

    ajuste

--- maestro/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://46.202.92.228:3000/api' // URL do servidor backend
-};
--- maestro/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api' // Desenvolvimento local (porta 3000)
-  // Para apontar para o servidor remoto, use: 'http://46.202.92.228:3000/api'
-};
