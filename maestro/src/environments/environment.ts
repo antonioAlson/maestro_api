0893cfeeb2a7baffd761fcf746050a99b4c889f3
Antonio Ailton Gonçalves <62522868+Antonio723@users.noreply.github.com>
2026-04-23 07:26:29 -0300

    fix: docker compose e enviroment

--- maestro/src/environments/environment.ts
+++ maestro/src/environments/environment.ts
@@ -1,5 +1,11 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api' // Desenvolvimento local (porta 3000)
+  apiUrl:'https://api1.orquestra-system.com/api' // URL do servidor backend
   // Para apontar para o servidor remoto, use: 'http://46.202.92.228:3000/api'
 };
+
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:3000/api' // Desenvolvimento local (porta 3000)
+//   // Para apontar para o servidor remoto, use: 'http://46.202.92.228:3000/api'
+// };
